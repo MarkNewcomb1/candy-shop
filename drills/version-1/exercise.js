49ae44415b05eb689b6c89c6a9ce5e4dbdaccee7
MarkNewcomb1 <wlmmn@hotmail.com>
2018-06-19 08:51:19 -0600

    all three passing

--- drills/version-1/exercise.js
+++ drills/version-1/exercise.js
@@ -1,6 +1,30 @@
+function candySalesToObject(arr) {
+    var result = arr.reduce(function(acc) {
+        acc.item = arr[0];
+        acc.price = arr[1];
+        acc.quantity = arr[2];
+        return acc;
+    }, {});
+    return result;
+}
+
+function salesDayToObject(dateAsString, obj) {
+    var result = obj[dateAsString].reduce(function(o, d) {
+        o.sales.push(candySalesToObject(d));
+        return o;
+    }, {date: dateAsString, sales: []});
+    return result;
+}
+
+function allSalesToArray(obj) {
+    var result = Object.keys(obj).map(function(date) {
+        return salesDayToObject(date, obj);
+    })
+    return result;
+}
 module.exports = {
     // Uncomment these functions as you write them
-    // candySalesToObject,
-    // salesDayToObject,
-    // allSalesToArray
+    candySalesToObject,
+    salesDayToObject,
+    allSalesToArray
 };
