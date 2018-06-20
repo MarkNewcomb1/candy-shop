1ecb9cca23b32a1d70650d8d639963e4ce02d2be
MarkNewcomb1 <wlmmn@hotmail.com>
2018-06-20 10:34:53 -0600

    all three passing

--- drills/version-3/exercise.js
+++ drills/version-3/exercise.js
@@ -1,6 +1,29 @@
+console.log();
+
+function getTotalItemSales(sales, itemID) {
+    return sales.filter(function(sale) {
+        return sale.itemId == itemID;
+    }).reduce(function(acc, sale) {
+        return acc += sale.quantity;
+    }, 0)
+}
+
+function addTotalSalesToItems(sales, items) {
+    return items.map(function(item){
+        item.quantity = getTotalItemSales(sales, item.id);
+        return item;
+    });
+}
+
+function addTotalValueToItems(sales, items) {
+    return addTotalSalesToItems(sales, items).map(function(item) {
+        item.totalValue = (item.quantity * item.price).toFixed(2);
+        return item;
+    })
+}
 module.exports = {
     // Uncomment these functions as you write them
-    // getTotalItemSales,
-    // addTotalSalesToItems,
-    // addTotalValueToItems
+    getTotalItemSales,
+    addTotalSalesToItems,
+    addTotalValueToItems
 }
