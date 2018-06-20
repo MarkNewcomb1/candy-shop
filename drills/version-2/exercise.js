5f1d6f11f33c2bbb2a5f9073bc861683c62d386f
MarkNewcomb1 <wlmmn@hotmail.com>
2018-06-20 09:47:36 -0600

    all three passing

--- drills/version-2/exercise.js
+++ drills/version-2/exercise.js
@@ -1,6 +1,26 @@
+function getItemById(list, itemId) {
+    return list.filter(function(item){
+        return item.id == itemId;
+    })[0];
+}
+
+function buildTransactions(sales, items) {
+    return sales.map(function(obj) {
+        return Object.assign(obj, getItemById(items, obj.itemId));
+    }).map(function(obj) {
+        delete obj.id;
+        return obj;
+    })
+}
+
+function getTransactionsByItemDescription(trans, descrip) {
+    return trans.filter(function(item){
+        return item.description === descrip;
+    })
+}
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
-    // buildTransactions,
-    // getTransactionsByItemDescription
+    getItemById,
+    buildTransactions,
+    getTransactionsByItemDescription
 }
